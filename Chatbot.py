bf758c58590e81e6f14ffbf7a40fc8d12265a8fc
thebeastjk <80276660+thebeastjk@users.noreply.github.com>
2023-04-22 00:14:43 -0700

    lots of changes

--- Chatbot.py
+++ Chatbot.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import openai
 import os
 import resources
+import json
 
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
@@ -9,38 +10,54 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 Textbased = True
 Audiobased = False
 
+# Load the conversation history from a file
+conversation_history = []
+if os.path.exists("conversation_history.json"):
+    with open("conversation_history.json", "r") as f:
+        conversation_history = json.load(f)
+
+# Generate a response to a new message
 def generate_response(prompt):
-    completions = openai.Completion.create(
-        engine = "text-curie-001",
-        prompt = prompt,
-        max_tokens = 150,
-        n = 1,
-        stop = None,
-        temperature = 0.5
+    # Get the most recent response from the conversation history
+    previous_response = conversation_history[-1]["response"] if conversation_history else ""
+
+    # Generate a new response using the OpenAI API
+    chat = openai.Completion.create(
+        engine="text-curie-001",
+        prompt=previous_response + "" +prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+        top_p=1,
+        frequency_penalty=0.5,
+        presence_penalty=0.5,
     )
 
-    message = completions.choices[0].text
-    return message.strip()
+    response = chat.choices.message["content"].strip()
+    # Store the message and response in the conversation history
+    #response = chat.choices[-1].text.strip()
+    conversation_history.append({"message": prompt, "response": response})
+    with open("conversation_history.json", "w") as f:
+        json.dump(conversation_history, f)
+    
 
-
+    return response
 
 while Textbased:
     user_input = input('You: ')
-    if (user_input == "Voice"):
+    if user_input.lower() == "voice":
         Textbased = False
         Audiobased = True
     else:
         response = generate_response(user_input)
-        print ("Bot: ", response)
+        print("Bot:", response)
 
 while Audiobased:
     user_input = resources.mic_loop()
-    if (user_input == "text"):
+    if user_input.lower() == "text":
         Textbased = True
         Audiobased = False
-    if (user_input == "Text"):
-        Textbased= True
-        Audiobased =False
     else:
         response = generate_response(user_input)
         resources.speak(response)
--- speech.py
+++ speech.py
@@ -1,7 +1,44 @@
-def speak(text):
-    import pyttsx3
-    engine = pyttsx3.init('sapi5')
-    engine.setProperty('rate', 175)
-    engine.setProperty('volume', 1.0)
-    engine.say(text)
-    engine.runAndWait()
+import os
+import openai
+from dotenv import load_dotenv
+import streamlit as st
+from streamlit_chat import message
+
+load_dotenv()
+openai.api_key= os.getenv('OPENAI_API_KEY')
+
+
+def generate_response(prompt):
+    completions = openai.Completion.create(
+        engine = "text-davinci-003",
+        prompt = prompt,
+        max_tokens = 1024,
+        n = 1,
+        stop = None,
+        temperature=0.5,
+    )
+    message = completions.choices[0].text
+    return message
+
+st.title("ChatBot: PyBot")
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+def get_text():
+    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
+    return input_text
+
+user_input = get_text()
+
+if user_input:
+    output=generate_response(user_input)
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1,-1,-1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i],is_user=True, key=str(i) +'user')
